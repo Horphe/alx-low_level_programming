1763a369ccdafebe24aa60a40b255e0f3d8ee013
Horphe <nzengue1994@gmail.com>
2021-06-17 12:25:47 -0700

    Print using Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#inclube <stdio.h>
+
+   /**
+    *main -prints the text
+    *Return : return the value 0 if exited properly
+    */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
