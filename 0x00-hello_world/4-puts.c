a2ed6c4bd5100d0b0e1e509ddded09e53e650358
Horphe <nzengue1994@gmail.com>
2021-06-17 13:21:07 -0700

    Print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 
-   /**
-    *main -prints the text
-    *Return: 0 if exited properly,non-zero otherwise
-    */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingul puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
