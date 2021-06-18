7bf4e9f2c125b7854ddd35ae9d9c1cb6bacedbd0
Horphe <nzengue1994@gmail.com>
2021-06-18 10:31:17 -0700

    if else statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,8 +18,8 @@ int main(void)
 
 	if (m > 5)
 		printf("%s %d is %d and is greater than 5\n", o, n, m);
-	else if (m==0)
-		printf("%s %d is %d and is greater than 0\n", o, n, m);
+	else if (m == 0)
+		printf("%s %d is %d and is 0\n", o, n, m);
 	else if (m < 6)
 		printf("%s %d is %d and is less than 6 and not 0\n", o, n, m);
 	return (0);
