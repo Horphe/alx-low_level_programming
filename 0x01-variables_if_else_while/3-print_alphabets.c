6d4af3686c460b1403ad4ea190d5997b5da450cd
Horphe <nzengue1994@gmail.com>
2021-06-18 11:46:41 -0700

    Print alphabet using Putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- *main - entry point
+ *main - Entry point
  *Description: Print lower and upper case a-z
- *Return : Return
+ *Return : Return the value (0)
  */
 int main(void)
 {
 	char ch;
 
-	for (ch ='a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
