635bb1ac4b201d93ae6ea5d880b57e89efdcf5d3
Horphe <nzengue1994@gmail.com>
2021-06-18 11:00:00 -0700

    If else statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *main Entry point
+ * main - Entry point
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
-int m;
-char o[] = "Last digit of";
+	int n;
+	int m;
+	char o[] = "Last digit of";
 
-srand(time(0));
-n = rand() -RAND_MAX / 2;
-m = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n % 10;
 
-if (m > 5)
-printf("%s %d is %d and is greater than 5\n", o, n, m);
-else if (m == 0)
-printf("%s %d is %d and is 0\n", o, n, m);
-else if (m < 6)
-printf("%s %d is %d and is less than 6 and not 0\n", o, n, m);
-return (0);
+	if (m > 5)
+		printf("%s %d is %d and is greater than 5\n", o, n, m);
+	else if (m == 0)
+		printf("%s %d is %d and is 0\n", o, n, m);
+	else if (m < 6)
+		printf("%s %d is %d and is less than 6 and not 0\n", o, n, m);
+	return (0);
 }
